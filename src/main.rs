b007cb1af9ebe25b38add1f0c2889a1adda019cf
jackyarndley <34801340+jackyarndley@users.noreply.github.com>
2019-07-24 10:23:51 +1200

    Added chapter 1, image writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let width: usize = 200;
+    let height: usize = 100;
+    let max_color: usize = 255;
+
+    // Begin PPM file header
+    let mut buffer = format!("P3\n{} {}\n 255\n", width, height);
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = ((i as f64 / width as f64) * max_color as f64) as u8;
+            let g = ((j as f64 / height as f64) * max_color as f64) as u8;
+            let b = (0.2 * max_color as f64) as u8;
+
+            buffer = format!("{}{} {} {}\n", buffer, r, g, b);
+        }
+    }
+
+    match fs::write("output.ppm", buffer) {
+        Err(_) => eprintln!("Could not generate the picture!"),
+        Ok(_) => ()
+    }
 }
