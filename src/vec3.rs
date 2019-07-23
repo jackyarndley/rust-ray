ae4329dcd10d1a3b1f2a837fc0d55e367c7386f3
jackyarndley <34801340+jackyarndley@users.noreply.github.com>
2019-07-24 10:43:54 +1200

    Added chapter 2, the vec3 struct

---
+++ src/vec3.rs
@@ -0,0 +1,90 @@
+use std::ops;
+
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 {
+            x,
+            y,
+            z
+        }
+    }
+
+    pub fn length(self) -> f64 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn squared_length(self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn unit(self) -> Vec3 {
+        self / self.length()
+    }
+
+    pub fn dot(self, v: Vec3) -> f64 {
+        self.x * v.x + self.y * v.y + self.z * v.z
+    }
+
+    pub fn cross(self, v: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * v.z - self.z * v.y,
+            y: self.z * v.x - self.x * v.z,
+            z: self.x * v.y - self.y * v.x
+        }
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, v: Vec3) -> Vec3 {
+        Vec3::new(self.x + v.x, self.y + v.y, self.z + v.z)
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, v: Vec3) -> Vec3 {
+        Vec3::new(self.x - v.x, self.y - v.y, self.z - v.z)
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, v: Vec3) -> Vec3 {
+        Vec3::new(self.x * v.x, self.y * v.y, self.z * v.z)
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn div(self, v: Vec3) -> Vec3 {
+        Vec3::new(self.x / v.x, self.y / v.y, self.z / v.z)
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, f: f64) -> Vec3 {
+        Vec3::new(self.x * f, self.y * f, self.z * f)
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, f: f64) -> Vec3 {
+        Vec3::new(self.x / f, self.y / f, self.z / f)
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        Vec3::new(-self.x, -self.y, -self.z)
+    }
+}
